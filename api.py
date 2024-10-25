92998ffd6826d79c70c3a550cc04b991b36aadc1
pluxury47 <pluxury47@gmail.com>
2024-10-25 15:39:03 +0300

    commit

---
+++ api.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+@app.route('/users/', methods=['GET'])
+def returnUserInfo():
+    id = request.args.get("id", type=int)
+    users = {
+    1: {"name": "Alex",
+            "age": 25
+    },
+    2: {"name": "Max",
+        "age": 28
+    },
+    3: {"name": "Egor",
+        "age": 15
+    }
+ }
+    return jsonify(users[id])
+pass
+if __name__ == "__main__":
+ app.run(debug=True)
